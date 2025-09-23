1add602099e55d54e49f2f57f16333dae50db5d7
Dinh-Son <vudinhso@gmail.com>
2025-09-23 16:54:25 +0700

    DWB navigator

--- install/rmitbot_bringup/share/rmitbot_bringup/launch/rmitbot.launch.py
+++ install/rmitbot_bringup/share/rmitbot_bringup/launch/rmitbot.launch.py
@@ -68,14 +68,14 @@ def generate_launch_description():
         ),
     )
     
+    # Careful and change the path of the navigation package --> lesson5_ws here
     nav2_in_new_terminal = ExecuteProcess(
     cmd=['xterm', '-hold', '-e', 'bash', '-lc',
          'source ~/rmitbot_v3/lesson5_ws/install/setup.bash; '
          'ros2 launch rmitbot_navigation nav.launch.py']
     )
-
     
-    # # Launch the navigation 3s after slamtoolbox, to make sure that a map is available
+    # Launch the navigation 10s after slamtoolbox, to make sure that a map is available
     navigation_delayed = TimerAction(
         period = 10., 
         actions=[nav2_in_new_terminal]
--- install/setup.sh
+++ install/setup.sh
@@ -34,38 +34,6 @@ _colcon_prefix_chain_sh_source_script() {
 COLCON_CURRENT_PREFIX="/opt/ros/jazzy"
 _colcon_prefix_chain_sh_source_script "$COLCON_CURRENT_PREFIX/local_setup.sh"
 
-# setting COLCON_CURRENT_PREFIX avoids relying on the build time prefix of the sourced script
-COLCON_CURRENT_PREFIX="/home/v120506/rmitbot_v2/lesson3_ws/install"
-_colcon_prefix_chain_sh_source_script "$COLCON_CURRENT_PREFIX/local_setup.sh"
-
-# setting COLCON_CURRENT_PREFIX avoids relying on the build time prefix of the sourced script
-COLCON_CURRENT_PREFIX="/home/v120506/rmitbot_v2/lesson4_ws/install"
-_colcon_prefix_chain_sh_source_script "$COLCON_CURRENT_PREFIX/local_setup.sh"
-
-# setting COLCON_CURRENT_PREFIX avoids relying on the build time prefix of the sourced script
-COLCON_CURRENT_PREFIX="/home/v120506/rmitbot_v2/lesson5_ws/install"
-_colcon_prefix_chain_sh_source_script "$COLCON_CURRENT_PREFIX/local_setup.sh"
-
-# setting COLCON_CURRENT_PREFIX avoids relying on the build time prefix of the sourced script
-COLCON_CURRENT_PREFIX="/home/v120506/rmitbot_v2/lesson6_ws/install"
-_colcon_prefix_chain_sh_source_script "$COLCON_CURRENT_PREFIX/local_setup.sh"
-
-# setting COLCON_CURRENT_PREFIX avoids relying on the build time prefix of the sourced script
-COLCON_CURRENT_PREFIX="/home/v120506/rmitbot_v3/lesson1_ws/install"
-_colcon_prefix_chain_sh_source_script "$COLCON_CURRENT_PREFIX/local_setup.sh"
-
-# setting COLCON_CURRENT_PREFIX avoids relying on the build time prefix of the sourced script
-COLCON_CURRENT_PREFIX="/home/v120506/rmitbot_v3/lesson2_ws/install"
-_colcon_prefix_chain_sh_source_script "$COLCON_CURRENT_PREFIX/local_setup.sh"
-
-# setting COLCON_CURRENT_PREFIX avoids relying on the build time prefix of the sourced script
-COLCON_CURRENT_PREFIX="/home/v120506/rmitbot_v3/lesson3_ws/install"
-_colcon_prefix_chain_sh_source_script "$COLCON_CURRENT_PREFIX/local_setup.sh"
-
-# setting COLCON_CURRENT_PREFIX avoids relying on the build time prefix of the sourced script
-COLCON_CURRENT_PREFIX="/home/v120506/rmitbot_v3/lesson4_ws/install"
-_colcon_prefix_chain_sh_source_script "$COLCON_CURRENT_PREFIX/local_setup.sh"
-
 
 # source this prefix
 # setting COLCON_CURRENT_PREFIX avoids relying on the build time prefix of the sourced script
